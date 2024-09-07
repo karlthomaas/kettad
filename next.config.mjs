fa7085d3855ad1cb163f70d8393aac4232457248
karlthomaas <karl-thomas@zink.ee>
2024-09-07 12:56:48 +0300

    chore: make project ouptut export

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 's3.amazonaws.com'
-      }
-    ]
-  }
-};
+        hostname: 's3.amazonaws.com',
+      },
+    ],
+  },
+}
 
-export default nextConfig;
+export default nextConfig
